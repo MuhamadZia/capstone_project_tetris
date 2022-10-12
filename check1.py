f9661fc7c49c97492d11578099fce7f7f93cdaea
MuhamadZia <ziahk68@gmail.com>
2022-10-12 16:35:53 +0700

    Percobaan input grafik option dan plotly

--- check1.py
+++ check1.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 # import seaborn as sns
 # import matplotlib.pyplot as plt
-
+import plotly.express as px
+import plotly.graph_objects as go
 
 st.title('Dampak Trend Elektrifikasi')
 st.write('Masa pandemi mempunyai banyak kisah yang tidak terduga dan era new normal.'+ 
@@ -25,4 +26,18 @@ st.write('Kendaraan listrik dicetuskan karena cost kendaraan yang lebih rendah d
 
 st.header('Asal sumber energi ')
 st.write('Sumber energi yang kita gunakan berasal dari berbagai macam. Bagian ini kita akan melihat sumber energi secara umum dan low-carbon')
+
 st.subheader('Sumber Energi Menyeluruh')
+elec_source = pd.read_csv('share-elec-by-source.csv')
+# elec_source['Year'] = pd.to_datetime(elec_source['Year'], format='%Y').dt.to_period('Y')
+elec_source['Year'] = elec_source['Year'].astype(str)
+col_filter, col_graph = st.columns(2)
+with col_filter:
+    option = st.selectbox('Pilih negara ?', set(elec_source.Entity))
+with col_graph:
+    chart_data = elec_source[(elec_source.Entity == option)][elec_source.columns[2:]]
+    chart_data = chart_data.rename(columns={'Year':'index'}).set_index('index')
+    st.line_chart(chart_data)
+
+fig = px.line(elec_source[elec_source.Entity == option], x='Year', y=elec_source.columns[3])
+st.plotly_chart(fig)
